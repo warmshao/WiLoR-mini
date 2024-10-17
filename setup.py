9b20bf5e80c512e0427b8e2e3eed45672694c26a
warmshao <wenshaoguo1026@gmail.com>
2024-10-17 09:08:37 +0800

    fix requirement bug

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@
 # @Project : WiLoR-mini
 # @FileName: setup.py
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Read requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# Separate GitHub dependencies
+github_deps = [req for req in requirements if req.startswith('git+')]
+regular_deps = [req for req in requirements if not req.startswith('git+')]
+
 setup(
     name='wilor_mini',
     version='1.0',
     description='WiLoR python package',
-    packages=[
-        'wilor_mini',
-        'wilor_mini.models',
-        'wilor_mini.pipelines',
-        'wilor_mini.utils'
-    ],
-    install_requires=requirements,
+    packages=find_packages(),
+    install_requires=regular_deps,
+    dependency_links=github_deps,
     python_requires='>=3.8',
     data_files=[]
 )
