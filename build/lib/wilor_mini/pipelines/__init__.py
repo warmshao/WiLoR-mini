8a39052d664850328887a3065c1267f71bfc17df
warmshao <wenshaoguo1026@gmail.com>
2024-10-27 12:27:41 +0800

    add predict with hand bboxes

--- build/lib/wilor_mini/pipelines/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/10/14
-# @Author  : wenshao
-# @Project : WiLoR-mini
-# @FileName: __init__.py.py
--- build/lib/wilor_mini/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/10/16
-# @Author  : wenshao
-# @Project : WiLoR-mini
-# @FileName: __init__.py.py
